6df5387c4bed634310d6abf1936bb90181876617
hackpulsar <kobylnyk.vladyslav@gmail.com>
2025-02-17 11:16:33 +0100

    add raylib to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include <raylib.h>
 
 int main() {
-    std::cout << "Hello, World!\n";
+    InitWindow(800, 600, "Realm Of Spells. The Online Duelling Card Game. by @TKGS");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Yooo what a gameplay", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
